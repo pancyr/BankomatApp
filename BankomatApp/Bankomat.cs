3a2a90f0f0936c612c3ed23783b59516d0471488
Кирилл Панаскин <kirpanaskin@gmail.com>
2021-03-19 12:04:24 +0300

    Добавьте файлы проекта.

---
+++ BankomatApp/Bankomat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankomatApp
+{
+    public class Bankomat
+    {
+        public Bankomat() { }
+
+        public Bankomat(string path)
+        {
+            this.Path = path;
+            
+        }
+
+        public string Path { get; set; }
+
+        private SortedDictionary<decimal, int> _banknotes;
+        public SortedDictionary<decimal, int> Banknotes
+        {
+            get
+            {
+                if (_banknotes == null)
+                {
+                    _banknotes = new SortedDictionary<decimal, int>();
+                    if (!Path.EndsWith("\\")) Path += "\\";
+                    string[] strings = File.ReadAllLines(this.Path + "atm.txt");
+                    foreach (string str in strings)
+                    {
+                        int pos = str.IndexOf(":");
+                        decimal nominal = Decimal.Parse(str.Substring(0, pos));
+                        int count = Int32.Parse(str.Substring(pos + 1, str.Length - (pos + 1)));
+                        Banknotes.Add(nominal, count);
+                    }
+                }
+                return _banknotes;
+            }
+        }
+
+        public Dictionary<decimal, int> TakeSumForClient(decimal sum)
+        {
+            Dictionary<decimal, int> result = new Dictionary<decimal, int>();
+            List<decimal> nominals = Banknotes.Keys.ToList();
+            decimal totalRest = sum;
+            while (totalRest > 0)
+            {
+                int lastIndex = Banknotes.Count - 1;
+                for (int i = lastIndex; i>=0; i--)
+                {
+                    decimal nom = nominals[i];
+                    if (nom <= totalRest)
+                    {
+                        if (result.ContainsKey(nom))
+                            result[nom]++;
+                        else result.Add(nom, 1);
+                        Banknotes[nom]--;
+                        if (Banknotes[nom] == 0)
+                        {
+                            Banknotes.Remove(nom);
+                            nominals.Remove(nom);
+                        }
+                        totalRest -= nom;
+                        break;
+
+                    }
+                    else if (i == 0)
+                    {
+                        return null;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ BankomatApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankomatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bankomat bankomat = new Bankomat("c:\\temp");
+            Console.WriteLine("Добрый день!");
+            Console.WriteLine("В банкомате имеются купюры");
+
+            foreach (decimal note in bankomat.Banknotes.Keys)
+                Console.WriteLine(note + " : " + bankomat.Banknotes[note]);
+
+            Console.WriteLine("Какая сумма Вам нужна?");
+
+            string sumString = Console.ReadLine();
+
+            try
+            {
+                decimal sumForClient = Decimal.Parse(sumString);
+                Dictionary<decimal, int> result = bankomat.TakeSumForClient(sumForClient);
+
+                if (result == null)
+                    Console.WriteLine("Мы не сможем выдать Вам эту сумму, извините!");
+                else
+                {
+                    Console.WriteLine("Заберите Ваши деньги!");
+                    foreach (decimal note in result.Keys)
+                        Console.WriteLine(note + " : " + result[note]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели некорректное значение!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
