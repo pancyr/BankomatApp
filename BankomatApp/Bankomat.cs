d97be0ecad76f269e82071f4dea6cbfe51de90e8
Кирилл Панаскин <kirpanaskin@gmail.com>
2021-03-19 18:32:17 +0300

    Исправленное

--- BankomatApp/Bankomat.cs
+++ BankomatApp/Bankomat.cs
@@ -34,22 +34,41 @@ namespace BankomatApp
                         int pos = str.IndexOf(":");
                         decimal nominal = Decimal.Parse(str.Substring(0, pos));
                         int count = Int32.Parse(str.Substring(pos + 1, str.Length - (pos + 1)));
-                        Banknotes.Add(nominal, count);
+                        _banknotes.Add(nominal, count);
                     }
                 }
                 return _banknotes;
             }
         }
 
+        private SortedDictionary<decimal, int> GetBanknotesCopy()
+        {
+            SortedDictionary<decimal, int> result = new SortedDictionary<decimal, int>();
+            foreach (decimal key in Banknotes.Keys)
+                result.Add(key, Banknotes[key]);
+            return result;
+        }
+
         public Dictionary<decimal, int> TakeSumForClient(decimal sum)
         {
+            int startPos = Banknotes.Count - 1;
+            while (startPos >= 0)
+            {
+                Dictionary<decimal, int> result = TakeSumForClient(sum, GetBanknotesCopy(), startPos);
+                if (result != null) return result;
+                startPos--;
+            }
+            return null;
+        }
+
+        private Dictionary<decimal, int> TakeSumForClient(decimal sum, SortedDictionary<decimal, int> bankList, int startPos)
+        {
             Dictionary<decimal, int> result = new Dictionary<decimal, int>();
-            List<decimal> nominals = Banknotes.Keys.ToList();
+            List<decimal> nominals = bankList.Keys.ToList();
             decimal totalRest = sum;
             while (totalRest > 0)
             {
-                int lastIndex = Banknotes.Count - 1;
-                for (int i = lastIndex; i>=0; i--)
+                for (int i = startPos; i>=0; i--)
                 {
                     decimal nom = nominals[i];
                     if (nom <= totalRest)
@@ -57,10 +76,10 @@ namespace BankomatApp
                         if (result.ContainsKey(nom))
                             result[nom]++;
                         else result.Add(nom, 1);
-                        Banknotes[nom]--;
-                        if (Banknotes[nom] == 0)
+                        bankList[nom]--;
+                        if (bankList[nom] == 0)
                         {
-                            Banknotes.Remove(nom);
+                            bankList.Remove(nom);
                             nominals.Remove(nom);
                         }
                         totalRest -= nom;
